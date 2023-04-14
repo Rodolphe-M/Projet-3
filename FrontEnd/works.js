660c0c2c5d037f251bdfd07482dd694dd051413f
Rodolphe Montaigut <rodolphe.montaigut@gmail.com>
2023-04-14 12:44:03 +0200

    Création de la boucle for

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -3,17 +3,22 @@ const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
 //Création des balises
-const article = works[0];
+for (let i = 0; i < works.length; i++) {
 
-const imageElement = document.createElement("img");
-imageElement.src = article.imageUrl;
+    const article = works[i];
+    //Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionGallery = document.querySelector(".gallery");
+    // Création d’une balise dédiée à un projet
+    const projetElement = document.createElement("article");
+    //Création des balises
+    const imageElement = document.createElement("img");
+    imageElement.src = article.imageUrl;
+    const titleElement = document.createElement("p");
+    titleElement.innerText = article.title;
 
-const titleElement = document.createElement("p");
-titleElement.innerText = article.title;
+    sectionGallery.appendChild(projetElement);
+    projetElement.appendChild(imageElement);
+    projetElement.appendChild(titleElement);
 
-//Rattachement des balises au DOM
-const sectionGallery = document.querySelector(".gallery");
+}
 
-
-sectionGallery.appendChild(imageElement);
-sectionGallery.appendChild(titleElement);
