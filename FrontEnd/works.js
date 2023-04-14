1678141abc28a0b279e53bfa704ef66f4108946f
Rodolphe Montaigut <rodolphe.montaigut@gmail.com>
2023-04-14 21:50:02 +0200

    activation bouton filtre code JS

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -4,6 +4,8 @@ const works = await reponseWorks.json();
 
 // Fonction qui génère les projets
 function genererProjets(works) {
+
+    document.querySelector(".gallery").innerHTML = "";
     
     for (let i = 0; i < works.length; i++) {
         const article = works[i];
@@ -29,17 +31,46 @@ function genererProjets(works) {
 // Premier affichage des projets
 genererProjets(works);
 
-// Récuperation des données catégories de l'API
-const reponseCategories = await fetch("http://localhost:5678/api/categories");
-const categories = await reponse.category.json();
 
 // bouton filtre objets
-const boutonFiltrer = document.querySelector(".btn-objet");
-
-boutonFiltrer.addEventListener("click", function () {
-    const projetElement = categories.filter(function (categories) {
-        return categories.name === "Objets";
-
-    });
+const boutonFiltrerObjets = document.querySelector("#btn-objets");
+boutonFiltrerObjets.addEventListener("click", function() {
+    const objetsFiltre = works.filter((work) => {
+        return work.category.name === "Objets"
+    })
+    // Regénération de la page avec les objets uniquement  
+    genererProjets(objetsFiltre);
 });
+
+// bouton filtre appartements
+const boutonFiltrerAppartements = document.querySelector("#btn-appartements");
+boutonFiltrerAppartements.addEventListener("click", function() {
+    const appartementsFiltre = works.filter((work) => {
+        return work.category.name === "Appartements"
+    })
+    // Regénération de la page avec les appartements uniquement  
+    genererProjets(appartementsFiltre);
+});
+
+// bouton filtre appartements
+const boutonFiltrerHotels = document.querySelector("#btn-hotels");
+boutonFiltrerHotels.addEventListener("click", function() {
+    const hotelsFiltre = works.filter((work) => {
+        return work.category.name === "Hotels & restaurants"
+    })
+    // Regénération de la page avec les appartements uniquement  
+    genererProjets(hotelsFiltre);
+});
+
+// bouton filtre appartements
+const boutonTous = document.querySelector("#btn-tous");
+boutonTous.addEventListener("click", function() {
+    const afficherTous = works.filter((work) => {
+        return genererProjets
+    })
+    // Regénération de la page avec les appartements uniquement  
+    genererProjets(afficherTous);
+});
+
+
 
