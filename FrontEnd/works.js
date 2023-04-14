5422c14b78879f4ba9492cf85b9302ea69f2de33
Rodolphe Montaigut <rodolphe.montaigut@gmail.com>
2023-04-14 19:04:59 +0200

    ajout function genererProjets

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,26 +1,45 @@
 // Récupération des travaux depuis l'API'
-const reponse = await fetch("http://localhost:5678/api/works");
-const works = await reponse.json();
+const reponseWorks = await fetch("http://localhost:5678/api/works");
+const works = await reponseWorks.json();
 
-//Création des balises
-for (let i = 0; i < works.length; i++) {
+// Fonction qui génère les projets
+function genererProjets(works) {
+    
+    for (let i = 0; i < works.length; i++) {
+        const article = works[i];
+        //Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionGallery = document.querySelector(".gallery");
+        // Création d’une balise dédiée à un projet
+        const projetElement = document.createElement("article");
+        //Création des balises
+        const imageElement = document.createElement("img");
+        imageElement.src = article.imageUrl;
+        const titleElement = document.createElement("p");
+        titleElement.innerText = article.title;
 
-    const article = works[i];
-    //Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionGallery = document.querySelector(".gallery");
-    // Création d’une balise dédiée à un projet
-    const projetElement = document.createElement("article");
-    //Création des balises
-    const imageElement = document.createElement("img");
-    imageElement.src = article.imageUrl;
-    const titleElement = document.createElement("p");
-    titleElement.innerText = article.title;
+        // On rattache la balise article dans la section gallery
+        sectionGallery.appendChild(projetElement);
+        // On rattache l'image et la description à la balise elementProjet (article)
+        projetElement.appendChild(imageElement);
+        projetElement.appendChild(titleElement);
 
-    // On rattache la balise article dans la section gallery
-    sectionGallery.appendChild(projetElement);
-    // On rattache l'image et la description à la balise elementProjet (article)
-    projetElement.appendChild(imageElement);
-    projetElement.appendChild(titleElement);
-
+    }
 }
+
+// Premier affichage des projets
+genererProjets(works);
+
+// Récuperation des données catégories de l'API
+const reponseCategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponse.category.json();
+
+// bouton filtre objets
+const boutonFiltrer = document.querySelector(".btn-objet");
+
+boutonFiltrer.addEventListener("click", function () {
+    const projetElement = categories.filter(function (categories) {
+        return categories.name === "Objets";
+
+    });
+});
 
