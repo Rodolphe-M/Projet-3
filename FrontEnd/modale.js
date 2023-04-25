5eb73224f6fb536d49a2167982c3c5dcea6bd294
Rodolphe Montaigut <rodolphe.montaigut@gmail.com>
2023-04-25 10:34:05 +0200

    modal 2 switch JS et CSS

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -48,3 +48,4 @@ window.addEventListener('keydown', function (e) {
         closeModal(e)
     }
 })
+
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -6,7 +6,7 @@ const works = await reponseWorks.json();
 function genererProjets(works) {
 
     document.querySelector(".gallery").innerHTML = "";
-    
+
     for (let i = 0; i < works.length; i++) {
         const article = works[i];
         //Récupération de l'élément du DOM qui accueillera les fiches
@@ -34,7 +34,7 @@ genererProjets(works);
 
 // bouton filtre objets
 const boutonFiltrerObjets = document.querySelector("#btn-objets");
-boutonFiltrerObjets.addEventListener("click", function() {
+boutonFiltrerObjets.addEventListener("click", function () {
     const objetsFiltre = works.filter((work) => {
         return work.category.name === "Objets"
     })
@@ -44,7 +44,7 @@ boutonFiltrerObjets.addEventListener("click", function() {
 
 // bouton filtre appartements
 const boutonFiltrerAppartements = document.querySelector("#btn-appartements");
-boutonFiltrerAppartements.addEventListener("click", function() {
+boutonFiltrerAppartements.addEventListener("click", function () {
     const appartementsFiltre = works.filter((work) => {
         return work.category.name === "Appartements"
     })
@@ -54,7 +54,7 @@ boutonFiltrerAppartements.addEventListener("click", function() {
 
 // bouton filtre appartements
 const boutonFiltrerHotels = document.querySelector("#btn-hotels");
-boutonFiltrerHotels.addEventListener("click", function() {
+boutonFiltrerHotels.addEventListener("click", function () {
     const hotelsFiltre = works.filter((work) => {
         return work.category.name === "Hotels & restaurants"
     })
@@ -64,7 +64,7 @@ boutonFiltrerHotels.addEventListener("click", function() {
 
 // bouton filtre appartements
 const boutonTous = document.querySelector("#btn-tous");
-boutonTous.addEventListener("click", function() {
+boutonTous.addEventListener("click", function () {
     const afficherTous = works.filter(() => {
         return genererProjets
     })
@@ -77,7 +77,7 @@ boutonTous.addEventListener("click", function() {
 // Selection des éléments à masquer
 const hiddenElements = document.querySelectorAll('.hidden');
 // Application de la propriété display: none aux éléments
-    hiddenElements.forEach(element => {
+hiddenElements.forEach(element => {
     element.style.display = 'none';
 })
 
@@ -88,19 +88,77 @@ function pageAdmin() {
     const token = localStorage.getItem('token');
     // Si présence token et id alors...
     if (userId && token) {
-      // On selectionne les classes hidden et not-hidden  
-      const elementsHidden = document.querySelectorAll('.hidden');
-      const elementsNotHidden = document.querySelectorAll('.not-hidden');
-      // On fait apparaitre les .hidden
-      elementsHidden.forEach(element => {
-        element.style.display = 'block';
-      });
-      // On masque les éléments .not-hidden
-      elementsNotHidden.forEach(element => {
-        element.style.display = 'none';
-      });
+        // On selectionne les classes hidden et not-hidden  
+        const elementsHidden = document.querySelectorAll('.hidden');
+        const elementsNotHidden = document.querySelectorAll('.not-hidden');
+        // On fait apparaitre les .hidden
+        elementsHidden.forEach(element => {
+            element.style.display = 'block';
+        });
+        // On masque les éléments .not-hidden
+        elementsNotHidden.forEach(element => {
+            element.style.display = 'none';
+        });
     }
-  }
-  // Appel de la fonction
-  pageAdmin();
+}
+// Appel de la fonction
+pageAdmin();
 
+  // Fonction qui génère les projets
+  function genererProjetsModal(works) {
+
+    document.querySelector(".modal-gallery").innerHTML = "";
+    
+    for (let i = 0; i < works.length; i++) {
+        const articleModal = works[i];
+        //Récupération de l'élément du DOM qui accueillera les fiches
+        const sectionGalleryModal = document.querySelector(".modal-gallery");
+        // Création d’une balise dédiée à un projet
+        const projetElementModal = document.createElement("article");
+        //Création des balises
+        const imageElementModal = document.createElement("img");
+        imageElementModal.src = articleModal.imageUrl;
+        const titleElementModal = document.createElement("p");
+        titleElementModal.innerText = articleModal.title = "éditer";
+
+        // On rattache la balise article dans la section gallery
+        sectionGalleryModal.appendChild(projetElementModal);
+        // On rattache l'image et la description à la balise elementProjet (article)
+        projetElementModal.appendChild(imageElementModal);
+        projetElementModal.appendChild(titleElementModal);
+
+    }
+}
+
+// Premier affichage des projets
+genererProjetsModal(works);
+
+
+// Function pour switcher d'une modale à l'autre
+function switchModal() {
+    const hiddenElements = document.querySelectorAll('.hidden-modal');
+    const notHiddenElements = document.querySelectorAll('.not-hidden-modal');
+    
+    // Afficher ou masquer les éléments de la modale
+    const switchElements = (elements, display, visibility) => {
+      elements.forEach(element => {
+        element.style.display = display;
+        element.style.visibility = visibility;
+      });
+    };
+    
+    // Ouvrir la modale 2 au clic
+    document.querySelector('#btn-add-work').addEventListener('click', () => {
+      switchElements(hiddenElements, 'block', 'visible');
+      switchElements(notHiddenElements, 'none', '');
+    });
+    
+    // Revenir en arrière au clic sur la flêche
+    document.querySelector('.js-modal-return').addEventListener('click', () => {
+      switchElements(hiddenElements, 'none', 'hidden');
+      switchElements(notHiddenElements, 'block', '');
+    });
+  }
+  
+  switchModal();
+  
